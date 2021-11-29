2091e37cc4e2f42806f68bc85bed0c9a0b333a52
gio <jojuagio7@gmail.com>
2021-11-29 15:15:36 +0400

    ss

--- js/script.js
+++ js/script.js
@@ -3,16 +3,20 @@ const registerSumbit = document.getElementById('register_submit')
 
 if (registerSumbit != null) {
 
-    registerSumbit.addEventListener('click', function e() {
+    registerSumbit.addEventListener('click', function (event) {
+        event.preventDefault();
+        let hasErrors = false;
 
-        //Field Values Start
+        const registerForm = document.getElementById('registration')
+
+        // Field Values Start
         const userName = document.getElementById('user_name').value
         const email = document.getElementById('email').value
         const age = document.getElementById('age').value
         const password = document.getElementById('password').value
         const passwordConfirm = document.getElementById('password_confirm').value
         const registerAgreement = document.getElementById('register_agreement')
-        //Field Values End
+        // Field Values End
 
         // Errors Variable Start
         const userNameError = document.getElementById("error_username")
@@ -29,6 +33,7 @@ if (registerSumbit != null) {
             errors.userName = 'Name should be minimum 3 letters'
             userNameError.innerHTML = errors.userName
             userNameError.style.color = 'red'
+            hasErrors = true
         } else {
             userNameError.innerHTML = ''
             localStorage.setItem('userName', userName)
@@ -38,6 +43,7 @@ if (registerSumbit != null) {
             errors.email = "Invalid Email"
             emailError.innerHTML = errors.email
             emailError.style.color = 'red'
+            hasErrors = true
         } else {
             emailError.innerHTML = ''
             localStorage.setItem('email', email)
@@ -47,6 +53,7 @@ if (registerSumbit != null) {
             errors.age = 'Your age should be at least 15'
             ageError.innerHTML = errors.age
             ageError.style.color = 'red'
+            hasErrors = true
         } else {
             ageError.innerHTML = ''
             localStorage.setItem('age', age)
@@ -56,6 +63,7 @@ if (registerSumbit != null) {
             errors.password = 'Passwords should be at least 8 symbol'
             passwordError.innerHTML = errors.password
             passwordError.style.color = 'red'
+            hasErrors = true
         } else {
             passwordError.innerHTML = ''
         }
@@ -64,6 +72,7 @@ if (registerSumbit != null) {
             errors.passwordConfirm = 'Passwords doesn\'t match'
             passwordConfirmError.innerHTML = errors.passwordConfirm
             passwordConfirmError.style.color = 'red'
+            hasErrors = true
         } else if (password === passwordConfirm && password.length > 7) {
             passwordConfirmError.innerHTML = ''
             localStorage.setItem('password', password)
@@ -73,17 +82,54 @@ if (registerSumbit != null) {
             errors.agreement = 'You need to agree terms'
             registerAgreementError.innerHTML = errors.agreement
             registerAgreementError.style.color = 'red'
+            hasErrors = true
         } else {
             registerAgreementError.innerHTML = ''
         }
 
-        return JSON.stringify(errors)
+        if (hasErrors === true) {
+            return hasErrors;
+        } else if (hasErrors === false){
+            registerForm.setAttribute('action', 'login.html')
+            registerForm.submit()
+        }
     })
 }
 
 if (loginSubmit != null) {
-    loginSubmit.addEventListener('click', function e() {
+    loginSubmit.addEventListener('click', function (event) {
+        event.preventDefault()
+        let hasErrors = false
+
+        const loginForm = document.getElementById('login')
+
         const loginUsername = document.getElementById('login_user').value
         const loginPassword = document.getElementById('login_password').value
+
+        const userNameLoginError = document.getElementById("error_login_username")
+        const userNamePasswordError = document.getElementById("error_login_password")
+
+        let errors = {}
+
+        if (loginUsername !== localStorage.getItem('userName')) {
+            errors.loginUserName = 'User doesn\'t exists'
+            userNameLoginError.innerHTML = errors.loginUserName
+            userNameLoginError.style.color = 'red'
+            hasErrors = true
+        }
+
+        if (loginPassword !== localStorage.getItem('password')) {
+            errors.loginPassword = 'Password doesn\'t match'
+            userNamePasswordError.innerHTML = errors.loginPassword
+            userNamePasswordError.style.color = 'red'
+            hasErrors = true
+        }
+
+        if (hasErrors === true) {
+            return hasErrors
+        } else if (hasErrors === false){
+            loginForm.setAttribute('action', 'profile.html')
+            loginForm.submit()
+        }
     })
 }
